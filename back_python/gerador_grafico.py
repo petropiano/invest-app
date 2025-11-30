f108746211daa608d7c4e74103c9c81c2777fc6a
andré <andre270506@gmail.com>
2025-11-29 21:48:35 -0300

    Update gerador_grafico.py

--- back_python/gerador_grafico.py
+++ back_python/gerador_grafico.py
@@ -21,4 +21,26 @@ def gerar_grafico_anual(ticker_simbolo):
         caminho_salvar = os.path.join(charts_dir, nome_arquivo_novo)
         caminho_relativo = f'charts/{nome_arquivo_novo}'
 
+        plt.figure(figsize=(10, 5))
+        plt.plot(data['Close'])
+        plt.title(f'Desempenho de {ticker_simbolo} (Último Ano)')
+        plt.ylabel('Preço de Fechamento (R$)')
+        plt.xlabel('Data')
+        plt.grid(True)
+        
+        plt.savefig(caminho_salvar)
+        plt.close() 
 
+        for filename in os.listdir(charts_dir):
+
+            if filename.startswith(f"{ticker_simbolo}_") and filename != nome_arquivo_novo:
+                try:
+                    os.remove(os.path.join(charts_dir, filename))
+                except Exception as e:
+                    print(f"Erro ao deletar gráfico antigo {filename}: {e}")
+        
+        return caminho_relativo
+
+    except Exception as e:
+        print(f"Erro ao gerar gráfico para {ticker_simbolo}: {e}")
+        return None
