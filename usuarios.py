ad2c06b3181883c81a55bc6b459089cb0bf8c059
camussi <camussireborn@gmail.com>
2025-12-09 17:55:45 -0300

    update_usuarios_login

--- usuarios.py
+++ usuarios.py
@@ -1,6 +1,6 @@
 import sqlite3
 import database
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 def cadastrar_usuario(nome_digitado, email_digitado, senha_digitada):
     nome = nome_digitado.strip().capitalize()
@@ -34,3 +34,27 @@ def cadastrar_usuario(nome_digitado, email_digitado, senha_digitada):
         return False
     except Exception as e:
         return False
+
+def login(email_digitado, senha_digitada):
+    
+    email = email_digitado.strip().lower()
+    
+    try:
+        db = sqlite3.connect(database.DB_NOME)
+        db.row_factory = sqlite3.Row 
+        cursor = db.cursor()
+        
+        login_sql = "SELECT * FROM usuarios WHERE email = ?"
+        cursor.execute(login_sql, (email,))
+        usuario_encontrado = cursor.fetchone()
+        db.close()
+        
+        if usuario_encontrado:
+            hash_salvo = usuario_encontrado['senha']
+            if check_password_hash(hash_salvo, senha_digitada):
+                return usuario_encontrado
+        return None
+        
+    except Exception as e:
+        print(f"Erro no login: {e}")
+        return None
